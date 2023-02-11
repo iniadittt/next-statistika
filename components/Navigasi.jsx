8fadce947f00054db1ef64c1168f69d6f4d89d1c
Aditya Bayu <97433258+iniadittt@users.noreply.github.com>
2023-02-11 07:06:09 +0700

    Add files via upload

--- components/Navigasi.jsx
+++ components/Navigasi.jsx
@@ -8,7 +8,7 @@ const Navigasi = () => {
     <div className='lg:fixed lg:w-full lg:py-4 lg:font-poppins lg:z-50 bg-[#0A2647] py-4 px-6'>
         <div className='lg:w-[1080px] lg:mx-auto lg:flex lg:flex-row lg:justify-between flex justify-between'>
             <Link href='/' className='lg:text-2xl font-bold text-white'>StatistikaProject</Link>
-            <ul className='lg:flex lg:flex-row lg:gap-7'>
+            <ul className='flex flex-row gap-4 lg:gap-7'>
               {
                 dataNavigasi.map((data, index) => {
                   return <Link key={index} href={`/${data}`} className='capitalize font-semibold text-white my-auto'>{data}</Link>
